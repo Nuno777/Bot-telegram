26c97d8bf03e5b5cfb39030c601d3f6407282200
Nuno Miguel <nm.albano20@gmail.com>
2024-06-05 17:51:48 +0100

    fix

--- keep_alive.py
+++ keep_alive.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, World!"
+    return "Bot is running!"
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8000))  # Use a porta especificada pela variável de ambiente PORT ou 5000 se não estiver definida
