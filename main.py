a856ae541d53719e581e41cc4f378382dc63bff0
Nuno Miguel <nm.albano20@gmail.com>
2024-06-10 03:33:47 +0100

    fix teste

--- main.py
+++ main.py
@@ -200,4 +200,5 @@ threading.Thread(target=start_bot).start()
 
 # Execute a aplicação Flask na porta especificada pela variável de ambiente PORT
 if __name__ == "__main__":
-   app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
